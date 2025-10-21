a092b6262c32649b96a44232361f473743bb6353
BitWiseNexus <adityarehpade1@gmail.com>
2025-10-21 21:34:43 +0530

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ const __dirname = path.resolve();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   credentials: true
 }));
 
